1fedca4b8596a2d0a18d1be1223ff93d128eeac7
marciusvic <marcio9769@gmail.com>
2024-09-27 15:23:13 -0300

    fix: putting a price in the book

--- backend-livraria/livraria/models.py
+++ backend-livraria/livraria/models.py
@@ -40,13 +40,14 @@ class User(AbstractBaseUser, PermissionsMixin):
         return self.email
 
 class Book(models.Model):
-    google_books_id = models.CharField(max_length=255, unique=True)  # Para relacionar com a API externa
+    google_books_id = models.CharField(max_length=255, unique=True)
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255, blank=True, null=True)
     published_date = models.CharField(max_length=100, blank=True, null=True)
     categories = models.CharField(max_length=255, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     thumbnail = models.URLField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True, default=9.99)
 
     def __str__(self):
         return self.title
